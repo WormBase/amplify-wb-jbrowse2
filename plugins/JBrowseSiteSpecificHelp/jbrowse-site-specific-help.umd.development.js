7d084e80744abe16133482d7998445e14f9de938
Scott Cain <scott+git@scottcain.net>
2023-07-14 13:56:31 -0700

    updating help plugin to have link to sharing view doc

--- plugins/JBrowseSiteSpecificHelp/jbrowse-site-specific-help.umd.development.js
+++ plugins/JBrowseSiteSpecificHelp/jbrowse-site-specific-help.umd.development.js
@@ -1625,7 +1625,9 @@
               React__default["default"].createElement("li", null,
                   React__default["default"].createElement(material.Link, { href: "https://blog.wormbase.org/2023/06/21/jbrowse-2-working-lists-bookmarks-and-sessions/", target: "_blank", rel: "noopener noreferrer" }, "Working with lists, bookmarks and sessions")),
               React__default["default"].createElement("li", null,
-                  React__default["default"].createElement(material.Link, { href: "https://blog.wormbase.org/2023/06/23/jbrowse-2-adding-your-own-data/", target: "_blank", rel: "noopener noreferrer" }, "Adding your own data"))),
+                  React__default["default"].createElement(material.Link, { href: "https://blog.wormbase.org/2023/06/23/jbrowse-2-adding-your-own-data/", target: "_blank", rel: "noopener noreferrer" }, "Adding your own data")),
+              React__default["default"].createElement("li", null,
+                  React__default["default"].createElement(material.Link, { href: "https://blog.wormbase.org/2023/07/14/jbrowse-2-sharing-views/", target: "_blank", rel: "noopener noreferrer" }, "Sharing views"))),
           React__default["default"].createElement("p", null,
               "The source code for this help widget plugin is on ",
               React__default["default"].createElement("a", { target: "_blank", href: "https://github.com/scottcain/jbrowse-site-specific-help" }, "GitHub"))));
