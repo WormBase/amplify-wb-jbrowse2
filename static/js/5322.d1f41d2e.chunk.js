c18db1ad4cecbe5bc055e14f0d70a874fa0fe0f8
Scott Cain <scott+git@scottcain.net>
2024-03-12 13:01:42 -0700

    bumping up to new JBrowse release

--- static/js/5322.d1f41d2e.chunk.js
+++
@@ -1,1 +0,0 @@
-"use strict";(globalThis.webpackChunk_jbrowse_web=globalThis.webpackChunk_jbrowse_web||[]).push([[5322],{75322:(s,e,t)=>{t.r(e),t.d(e,{default:()=>l.ZP,getListItemUtilityClass:()=>a.o,listItemClasses:()=>a.Z});var l=t(74780),a=t(30243)}}]);
